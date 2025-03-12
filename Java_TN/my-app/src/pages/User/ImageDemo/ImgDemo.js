fdde69a5c0c94d216a18a56221d64832434ab565
nhutjv <141827104+nhutjv@users.noreply.github.com>
2025-03-12 16:52:05 +0700

    Delete Java_TN directory

--- Java_TN/my-app/src/pages/User/ImageDemo/ImgDemo.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import StorageImage from '../../../components/user/StorageImageText/StorageImage'
-export default class ImgDemo extends Component {
-  render() {
-    return (
-      <StorageImage/>
-    )
-  }
-}
